fb36d09314af62bf3561f9d74a70b2d097cb313d
Diego Sullcaray Ramos <diego.sullcaray@confianza.pe>
2026-07-14 16:03:11 -0500

    feat: add sistemas-detalle and sistemas-list components with HTML and CSS files

---
+++ src/app/pages/modules/admin/components/gestion-sistemas/items/sistemas-detalle/sistemas-detalle.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sistemas-detalle',
+  imports: [],
+  templateUrl: './sistemas-detalle.component.html',
+  styleUrl: './sistemas-detalle.component.css',
+})
+export class SistemasDetalleComponent {}
---
+++ src/app/pages/modules/admin/components/gestion-sistemas/items/sistemas-list/sistemas-list.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sistemas-list',
+  imports: [],
+  templateUrl: './sistemas-list.component.html',
+  styleUrl: './sistemas-list.component.css',
+})
+export class SistemasListComponent {}
