b9ed904ddb3a1a44be96836eedca4e4738894d89
Diego Sullcaray Ramos <diego.sullcaray@confianza.pe>
2026-08-18 14:01:16 -0500

    cambios

--- src/app/pages/modules/home/components/inicio/inicio.component.ts
+++ src/app/pages/modules/home/components/inicio/inicio.component.ts
@@ -1,24 +1,21 @@
 import { Component, computed, inject } from '@angular/core';
-import { RouterLink } from '@angular/router';
-import { NgIconComponent, provideIcons } from '@ng-icons/core';
+import {provideIcons } from '@ng-icons/core';
 import { lucideTrophy } from '@ng-icons/lucide';
 import { ShellStateService } from '../../../../../core/services/shell-state.service';
-import { KaypachaService } from '../../../ranking-k/services/kaypacha.service';
+
 
 @Component({
   selector: 'app-inicio',
   standalone: true,
-  imports: [RouterLink, NgIconComponent],
+  imports: [],
   viewProviders: [provideIcons({ lucideTrophy })],
   templateUrl: './inicio.component.html',
   styleUrl: './inicio.component.css',
 })
 export class InicioComponent {
   protected readonly shell = inject(ShellStateService);
-  protected readonly kaypacha = inject(KaypachaService);
 
   constructor() {
-    this.kaypacha.cargarCategorias();
   }
 
   protected readonly primerNombre = computed(() => {
