05923dc1a054a7396d8facd33271dea599dc797f
Diego Sullcaray Ramos <diego.sullcaray@confianza.pe>
2026-08-22 19:26:11 -0500

    implemenatcion  de modulo de captaciones

---
+++ src/app/pages/modules/reportes/components/actividad-diaria/components/Captaciones/captacion-por-canal/captacion-por-canal.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-captacion-por-canal',
+  imports: [],
+  templateUrl: './captacion-por-canal.component.html',
+  styleUrl: './captacion-por-canal.component.css',
+})
+export class CaptacionPorCanalComponent {}
